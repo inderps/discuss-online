a3e027823121451d2aaccbdcf21d21768f992f12
IPS <inderps@thoughtworks.com>
2012-06-30 14:45:53 +0530

    app structure added

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<title>Discuss Online</title> 
+<link type="text/css" rel="stylesheet"  href="scripts/css/style.css">
+<!-- <link rel="icon" type="image/png" href="http://c551207.r7.cf2.rackcdn.com/img/favicon.png">  -->
+</head> 
+<body> 
+	
+</body> 
+</html> 
